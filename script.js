47485dec0bfa005d5b3d62b1d28dcfafdf08d524
mateusviniciusotako2023 <146885939+mateusviniciusotako2023@users.noreply.github.com>
2025-07-31 11:18:34 -0300

    missao

--- script.js
+++ script.js
@@ -3,5 +3,92 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-Aternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+const perguntas = [
+    {
+        enunciado: "qual pokemon o ash capturou primeiro?",
+        alternativas: [
+            {
+                texto:"caterpi",
+                afirmação:"Resposta correta",
+                pontos:1
+            },
+            {
+                texto:"kakuna",
+                afirmação:"Resposta errada",
+                pontos:0
+            }
+        ]
+    },
+     {
+        enunciado: "quando o ash se atrasou qual pokémon o ash pegou primeiro?",
+        alternativas: [
+            {
+                texto:"pikachu",
+                afirmação:"Resposta correta",
+                pontos:1
+            },
+            {
+                texto:"charmander",
+                afirmação:"Resposta errada",
+                pontos:0
+            }
+        ]
+    },
+    {
+        enunciado: "qual jogo de pokemon foi lançado primeiro?",
+        alternativas: [
+            {
+                texto:"red e grem",
+                afirmação:"Resposta correta",
+                pontos:1
+            },
+            {
+                texto:"yellow soul cristal",
+                afirmação:"Resposta errada",
+                pontos:0
+            }
+        ]
+    },
+    {
+        enunciado: "qual foi o primeiro remake de pokémon?",
+        alternativas: [
+            {
+                texto:"pokemon fire red e leaf grem",
+                afirmação:"Resposta correta",
+                pontos:1
+            },
+            {
+                texto:"soul silver heart gold",
+                afirmação:"Resposta errada",
+                pontos:0
+            }
+        ]
+    },
+    {
+        enunciado: "qual foi o ano que lansou pokemon red e grem?",
+        alternativas: [
+            {
+                texto:"1996",
+                afirmação:"Resposta correta",
+                pontos:1
+            },
+            {
+                texto:"1998",
+                afirmação:"Resposta errada",
+                pontos:0
+            }
+        ]
+    },
+]
 
+let atual=0;
+let perguntaAtual;
+let historiafinal="";
+let pontos=0;
 
+function mostraPergunta(){
+    perguntaAtua=perguntas[atual];
+    caixaAlternativas.textContent=perguntaAtual.enunciado;
+    mostraAlternativas();
+
+}
